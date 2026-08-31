40eccef8cc23ccfa4c913a7325a8481ccaddb62d
David Sagan <dcs16@cornell.edu>
2026-08-31 13:44:38 -0400

    Remove author list.

--- source/conf.py
+++ source/conf.py
@@ -8,7 +8,7 @@
 
 project = 'Particle Accelerator Language Standard (PALS)'
 copyright = '2025, under CC-BY 4.0 License'
-author = 'Jean-Luc Vay, David Sagan, Chad Mitchell, Axel Huebl, David Bruhwihler, Christopher Mayes, Eric Stern, Daniel Winklehner, Michael Ehrlichman, Martin Berz, Giovanni Iadarola, Ji Qiang, Edoardo Zoni, Laurent Deniau, et al.'
+## author = 'Jean-Luc Vay, David Sagan, Chad Mitchell, Axel Huebl, David Bruhwihler, Christopher Mayes, Eric Stern, Daniel Winklehner, Michael Ehrlichman, Martin Berz, Giovanni Iadarola, Ji Qiang, Edoardo Zoni, Laurent Deniau, et al.'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
